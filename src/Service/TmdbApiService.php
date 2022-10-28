ed6c00a83145606160e0e4d0b89dd621d6b4e1d6
Bang DAI <bang.dai2@gmail.com>
2022-10-28 14:43:55 +0200

    REST API ok

--- src/Service/TmdbApiService.php
+++ src/Service/TmdbApiService.php
@@ -2,11 +2,16 @@
 
 namespace App\Service;
 
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class TmdbApiService
 {
+    const IMG_BASE_URL_KEY = 'img_base_url_key';
+    const IMG_BACKDROP_SIZE = 'w1280';
+    const IMG_POSTER_SIZE = 'w154';
     private $client;
 
     public function __construct(HttpClientInterface $tmdbApiClient)
@@ -47,7 +52,14 @@ class TmdbApiService
 
     public function getInfo(int $id): array
     {
-        return $this->callApi('movie/' . $id, ['append_to_response' => 'videos']);
+        $res = $this->callApi('movie/' . $id, ['append_to_response' => 'videos']);
+        if (empty($res)) {
+            return $res;
+        }
+        $res['backdrop_path'] = sprintf('%s%s%s', $this->getImgBaseUrl(), self::IMG_BACKDROP_SIZE, $res['backdrop_path']);
+        $res['poster_path'] = sprintf('%s%s%s', $this->getImgBaseUrl(), self::IMG_POSTER_SIZE, $res['poster_path']);
+
+        return $res;
     }
 
     private function callApi(string $url, array $query = [], string $lang = 'fr-FR'): array
@@ -59,5 +71,23 @@ class TmdbApiService
         $response = $this->client->request('GET', $url, $options);
 
         return ($response->getStatusCode() === Response::HTTP_OK) ? $response->toArray() : [];
+    }
+
+    /**
+     * Get the image base url and cache it for 24h
+     */
+    private function getImgBaseUrl(): string
+    {
+        $cache = new FilesystemAdapter();
+        $value = $cache->get(self::IMG_BASE_URL_KEY, function (ItemInterface $item) {
+            $item->expiresAfter(3600 * 24);
+            $res = $this->callApi('configuration');
+            if (!empty($res)) {
+                return $res['images']['secure_base_url'];
+            }
+            return '';
+        });
+
+        return $value;
     }
 }
